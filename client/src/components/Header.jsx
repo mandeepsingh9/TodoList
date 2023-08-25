9a1f59fe0f57a52ee284f7e0eef9695bcaf85d72
mandeep kumar <minturajmdb1999@gmail.com>
2023-08-25 12:44:09 +0530

    Added my project

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <div className="waviy">
+      <span style={{'--i': 1}}>T</span>
+      <span style={{'--i': 2}}>O</span>
+      <span style={{'--i': 3}}>D</span>
+      <span style={{'--i': 4}}>O</span>
+      <span style={{'--i': 5}}>L</span>
+      <span style={{'--i': 6}}>I</span>
+      <span style={{'--i': 7}}>S</span>
+      <span style={{'--i': 8}}>T</span>
+      </div>
+
+        </header>
+    )
+}
+
+export default Header;
